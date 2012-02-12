0ab555542b676e39ef65eadd7f658a458cfd09ff
Eric MacAdie <Eric@MacAdie.net>
2012-02-11 23:16:31 -0600

    In authentication branch

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,2 +1,22 @@
 module SessionHelper
+    def sign_in(user)
+        cookies.permanent.signed[:remember_token] = [user.id]
+        self.current_user = user
+    end
+    
+    def current_user=(user)
+        @current_user = user
+    end
+    
+    def current_user
+        @current_user ||= user_from_remember_token
+    end
+    
+    private
+    def user_from_remember_token
+        User.authenticate(*remember_token)
+    end
+    def remember_token
+        cookies.signed[:remember_token] || [nil, nil]
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ActiveRecord::Base
   belongs_to :school
   
   has_secure_password
-  
+    
+  def authenticate(sub_username, submitted_password)
+      user = User.find_by_username( sub_username)
+      # if user.password != submitted_password
+      #     user = nil
+      
+      # end
+            
+  end
+    
 end
