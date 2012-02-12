1df286a376ec957fbf7e85eadc7e06006810ee0d
Eric MacAdie <Eric@MacAdie.net>
2012-02-12 05:00:10 -0600

    Okay

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :articles
   belongs_to :school
   
-  has_secure_password
+  # has_secure_password
     
   def authenticate(sub_username, submitted_password)
       user = User.find_by_username( sub_username)
